1bf9a23c33e2711fba0b57c26bba7e8ae1b98088
Nills Franssens <nills.franssens@gmail.com>
2020-11-06 15:11:39 -0800

    logging again

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ admission_controller = Flask(__name__)
 @admission_controller.route('/mutate/deployments', methods=['POST'])
 def deployment_webhook_mutate():
     request_info = request.get_json()
-    logging.info("request_info")
+    logging.warning(request_info)
     return admission_response_patch(True, "Adding allow label", json_patch = jsonpatch.JsonPatch([{"op": "add", "path": "/metadata/labels/allow", "value": "yes"}]))
 def admission_response_patch(allowed, message, json_patch):
     base64_patch = base64.b64encode(json_patch.to_string().encode("utf-8")).decode("utf-8")
