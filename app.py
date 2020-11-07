4daa7e7761c22b3c321d59d7a3b60c98b4849aa8
Nills Franssens <nills.franssens@gmail.com>
2020-11-06 16:20:57 -0800

    fixed admission controller

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, jsonify
+from kubernetes import client, config
+
 import base64
 import logging
 import jsonpatch
@@ -6,9 +8,20 @@ admission_controller = Flask(__name__)
 @admission_controller.route('/mutate/pods', methods=['POST'])
 def deployment_webhook_mutate():
     request_info = request.get_json()
-    logging.info("Got request")
-    os_in_pod = request_info.request.object.spec.nodeSelector
-    logging.warning("OS is {}".format(os_in_pod))
+    logging.warning("Got request")
+    
+    # Get OS from Pod
+    
+    try:
+        os_in_pod = request_info["request"]["object"]["spec"]["nodeSelector"]["kubernetes.io/os"]
+    except KeyError as e:
+        logging.warning("OS not set in node selector. Assuming default Linux.")
+        os_in_pod = 'linux'
+    logging.warning("OS in pod is {}".format(os_in_pod))
+
+    # Get request from Pod
+    logging.warning(request_info["request"]["object"]["spec"])        
+
     return admission_response_patch(True, "Adding allow label", json_patch = jsonpatch.JsonPatch([{"op": "add", "path": "/metadata/labels/allow", "value": "yes"}]))
 def admission_response_patch(allowed, message, json_patch):
     base64_patch = base64.b64encode(json_patch.to_string().encode("utf-8")).decode("utf-8")
